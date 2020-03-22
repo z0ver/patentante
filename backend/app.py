e935b63244cab19ee8ea9f85fc101639bf139478
wuest.thomas@gmx.de <wuest.thomas@gmx.de>
2020-03-22 20:36:44 +0100

    reorganized APIs in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,27 @@
+"""
+Collection of different APIs which access the MySQL database in the backend
+The APIs below are grouped by type.
+To create a new API, just use these templates.
+Database calls are done using functions defined in the db.py module
+
+POST(like)
+@app.route('/my/endpoint',methods=['POST'])
+def some_random_name():
+    received_body = request.get_json()
+    value = received_body['value_in_json']
+    ...
+    result_dictionary = ...
+    response_valid_request(result_dictionary)
+
+GET(like)
+@app.route('/my/endpoint',methods=['GET'])
+def some_random_name():
+    value = request.args.get('value_in_url')
+    ...
+    result_dictionary = ...
+    response_valid_request(result_dictionary)
+"""
+
 from datetime import datetime
 
 import simplejson as json
@@ -12,7 +36,9 @@ CORS(app)
 app.secret_key = secret_key
 app.config['SESSION_TYPE'] = 'filesystem'
 
+#-------------------Section with predefined requests for standardization---------------
 
+#Function returning message "Invalid request" from this server
 def response_invalid_request():
     """
     Response to an invalid request.
@@ -26,7 +52,7 @@ def response_invalid_request():
                     status=400,
                     mimetype="application/json")
 
-
+#Function returning "Unauthorized request" from this server
 def response_unauthorized_request():
     """
     Response to an unauthorized request.
@@ -41,6 +67,7 @@ def response_unauthorized_request():
                     mimetype="application/json")
 
 
+#Function returning "Valid Request" from this server
 def response_valid_request(data):
     """
     Response to a valid request.
@@ -67,6 +94,8 @@ def response_valid_request(data):
                     mimetype="application/json")
 
 
+#-----------------------APIs for User management--------------------
+
 # login user
 @app.route('/user/login', methods=['POST'])
 def login_user():
@@ -92,6 +121,7 @@ def register_customer():
         lastname = data.get('lastname')
         phone_number = data.get('phone_number')
         password = data.get('password')
+        #hash password before insert in database
         passwordHash = get_hashed_password(password.encode('utf8'))
         result = insertUser(email, firstname, lastname, phone_number, passwordHash, False)
         if result.get('success'):
@@ -99,6 +129,26 @@ def register_customer():
         else:
             return response_invalid_request()
 
+# register a new vendor
+@app.route('/user/vendor/register', methods=['POST'])
+def register_vendor():
+    if request.method == 'POST':
+        data = request.get_json()
+        email = data.get('email')
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        phone_number = data.get('phonenumber')
+        password = data.get('password')
+        passwordHash = get_hashed_password(password.encode('utf8'))
+        result = insertUser(email, firstname, lastname, phone_number, passwordHash, True)
+        if result.get('success'):
+            return response_valid_request({"user_id": result.get('inserted_id')})
+        else:
+            return response_invalid_request()
+
+
+
+#------------------------Vendor-specific APIs------------------------
 
 # Issue a new coupon or retrieve all of a vendor
 @app.route('/user/vendor/coupon', methods=['POST', 'GET'])
@@ -124,24 +174,70 @@ def vendor_coupon():
         } for database_response in database_responses]
         return response_valid_request(final_response)
 
-
-# register a new vendor
-@app.route('/user/vendor/register', methods=['POST'])
-def register_vendor():
+# Get or create a new offer for a vendor
+@app.route('/user/vendor/offer', methods=['POST', 'GET'])
+def vendor_offer():
     if request.method == 'POST':
         data = request.get_json()
-        email = data.get('email')
-        firstname = data.get('firstname')
-        lastname = data.get('lastname')
-        phone_number = data.get('phonenumber')
-        password = data.get('password')
-        passwordHash = get_hashed_password(password.encode('utf8'))
-        result = insertUser(email, firstname, lastname, phone_number, passwordHash, True)
+        shop_id = data.get('shop_id')
+        offerType = data.get('offerType')
+        name = data.get('name')
+        description = data.get('description')
+        value = data.get('value')
+        result = createOffer(shop_id=shop_id, offerType=offerType, name=name,
+                             description=description, value=value)
         if result.get('success'):
-            return response_valid_request({"user_id": result.get('inserted_id')})
+            return response_valid_request({"offer_id": result.get("inserted_id")})
         else:
             return response_invalid_request()
+    elif request.method == 'GET':
+        shop_id = request.args.get('shop_id')
+        return response_valid_request(getOffersByShopID(shop_id))
 
+# add a new shop for a owner and get all shops for an owner
+@app.route('/user/vendor/shops', methods=['POST', 'GET'])
+def register_shop_and_retrieve_by_owner():
+    if request.method == 'POST':
+        data = request.get_json()
+        owner_id = data.get('owner_id')
+        street = data.get('street')
+        zip_code = data.get('zip_code')
+        city = data.get('city')
+        website_url = data.get('website_url')
+        phone_number = data.get('phone_number')
+        name = data.get('name')
+        logo_url = data.get('logo_url')
+        description_short = data.get('description_short')
+        description = data.get('description')
+        result = insertShopDetails(owner_id, street, zip_code, city, website_url, phone_number, name, logo_url,
+                                   description_short, description)
+        if result.get('success'):
+            return response_valid_request({"shop_id": result.get('inserted_id')})
+        else:
+            return response_invalid_request()
+    elif request.method == 'GET':
+        owner_id = request.args.get('owner_id')
+        database_responses = getShopsByOwner(owner_id)
+        final_response = [{
+            "shop_id": database_response.get("shop_id"),
+            "address": {
+                "street": database_response.get("street"),
+                "zip_code": database_response.get("zip_code"),
+                "city": database_response.get("city"),
+                "website_url": database_response.get("website_url"),
+                "phone_number": database_response.get("phone_number")
+            },
+            "information_basic": {
+                "name": database_response.get("name"),
+                "logo_url": database_response.get("logo_url"),
+                "description_short": database_response.get("description_short")
+            },
+            "description": database_response.get("description"),
+        } for database_response in database_responses]
+        return response_valid_request(final_response)
+
+
+#----------------------------Shop-relevant functions---------------------
 
 # Get all shops with a specific name
 @app.route('/shops/name', methods=['GET'])
@@ -209,74 +305,14 @@ def get_coupons_by_shop():
     return response_valid_request(getCouponsByShopID(shop_id))
 
 
+
+#-------------------------------Coupon management------------------------
+
 # Get all coupons by a customer
 @app.route('/user/customer/coupons', methods=['GET'])
 def get_coupons_by_customer():
     customer_id = request.args.get('customer_id')
     return response_valid_request(getCouponsByCustomerID(customer_id))
-
-
-@app.route('/user/vendor/offer', methods=['POST', 'GET'])
-def vendor_offer():
-    if request.method == 'POST':
-        data = request.get_json()
-        shop_id = data.get('shop_id')
-        offerType = data.get('offerType')
-        name = data.get('name')
-        description = data.get('description')
-        value = data.get('value')
-        result = createOffer(shop_id=shop_id, offerType=offerType, name=name,
-                             description=description, value=value)
-        if result.get('success'):
-            return response_valid_request({"offer_id": result.get("inserted_id")})
-        else:
-            return response_invalid_request()
-    elif request.method == 'GET':
-        shop_id = request.args.get('shop_id')
-        return response_valid_request(getOffersByShopID(shop_id))
-
-
-# add a new shop for a owner and get all shops for an owner
-@app.route('/user/vendor/shops', methods=['POST', 'GET'])
-def register_shop_and_retrieve_by_owner():
-    if request.method == 'POST':
-        data = request.get_json()
-        owner_id = data.get('owner_id')
-        street = data.get('street')
-        zip_code = data.get('zip_code')
-        city = data.get('city')
-        website_url = data.get('website_url')
-        phone_number = data.get('phone_number')
-        name = data.get('name')
-        logo_url = data.get('logo_url')
-        description_short = data.get('description_short')
-        description = data.get('description')
-        result = insertShopDetails(owner_id, street, zip_code, city, website_url, phone_number, name, logo_url,
-                                   description_short, description)
-        if result.get('success'):
-            return response_valid_request({"shop_id": result.get('inserted_id')})
-        else:
-            return response_invalid_request()
-    elif request.method == 'GET':
-        owner_id = request.args.get('owner_id')
-        database_responses = getShopsByOwner(owner_id)
-        final_response = [{
-            "shop_id": database_response.get("shop_id"),
-            "address": {
-                "street": database_response.get("street"),
-                "zip_code": database_response.get("zip_code"),
-                "city": database_response.get("city"),
-                "website_url": database_response.get("website_url"),
-                "phone_number": database_response.get("phone_number")
-            },
-            "information_basic": {
-                "name": database_response.get("name"),
-                "logo_url": database_response.get("logo_url"),
-                "description_short": database_response.get("description_short")
-            },
-            "description": database_response.get("description"),
-        } for database_response in database_responses]
-        return response_valid_request(final_response)
 
 
 # Update value of coupon

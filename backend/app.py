3a7550e4622fa9624430dd74808fe789f307b1f3
wuest.thomas@gmx.de <wuest.thomas@gmx.de>
2020-03-22 16:54:48 +0100

    added imports for db module in main module to check if this also runs in an error in WSGI

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,16 @@
 from datetime import datetime
 
 import simplejson as json
-from flask import Flask, redirect, url_for, session, render_template, request, Response
-from flask_login import LoginManager, login_user, login_required
-from flask_session import Session
+from flask import Flask, request, Response
 
 from config import secret_key
+import bcrypt
+import mysql.connector
 from db import *
-from model import User
 
 app = Flask(__name__)
 app.secret_key = secret_key
 app.config['SESSION_TYPE'] = 'filesystem'
-
-Session(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login_page'
 
 
 def response_invalid_request():
@@ -72,51 +65,6 @@ def response_valid_request(data):
     return Response(response=json.dumps(_response, sort_keys=True, indent=4),
                     status=200,
                     mimetype="application/json")
-
-
-# Main page, ToDo: create main page template
-@app.route('/')
-@login_required
-def index():
-    return "Welcome"
-
-
-# compare the user id with the known sessions
-@login_manager.user_loader
-def load_user(id):
-    if 'users' in session.keys():
-        if int(id) in session['users'].keys():
-            return session['users'][int(id)]
-        else:
-            return None
-    else:
-        session['users'] = {}
-    return id
-
-
-# Login page for the user
-@app.route('/login')
-def login_page():
-    return render_template("login_form.html")
-
-
-# Page where the data entered in the login page gets validated
-# If validation not successful, it moves back to the login page
-@app.route('/logged-in', methods=['POST'])
-def loggedin_page():
-    username = request.form["username"]
-    password = request.form["password"]
-    # ToDo: database check if we know this user and the password is correct
-    if (password == '1234' and username == 'test'):
-        session.clear()
-        if not 'users' in session.keys():
-            session['users'] = {}
-        user = User({"id": 0, "name": "Horst"})
-        session['users'][user.id] = user
-        login_user(user)
-        return redirect(url_for('index'))
-    else:
-        return redirect(url_for('login_page'))
 
 
 # login user
